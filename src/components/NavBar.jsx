c1edb1930d07afac2d6088eba78bf52cb920f751
sivanesh2001 <84060837+sivanesh2001@users.noreply.github.com>
2023-07-18 13:23:43 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
   };
 
   return (
-    <div className="navbar bg-neutral text-neutral-content">
+    <div className="navbar"  style={{ backgroundColor: "#041E42", color: "white" }}>
       <div className="containerWrap flex justify-between">
         <a className="btn btn-ghost normal-case text-xl">Chatbox</a>
         {currentUser ? (
